a0ccd692e0588354f31e33489906b9e2cbf7cbff
h2812 <h2812.me@gmail.com>
2021-10-30 00:12:10 +0530

    fix: refactor hard coded connection string to Heroku config var

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://one:onetwothree@cluster0.5knhd.mongodb.net/d2c?retryWrites=true&w=majority";
+const connection = process.env.MONGODB_URI;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
